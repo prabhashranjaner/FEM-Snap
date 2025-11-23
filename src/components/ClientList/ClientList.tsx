57ff984f440aa37a695d499942a1854aaaa372c5
Prabhash Ranjan <prabhashranjaner@gmail.com>
2025-11-23 13:26:25 +0530

    minor bug

--- src/components/ClientList/ClientList.tsx
+++ src/components/ClientList/ClientList.tsx
@@ -6,7 +6,7 @@ const ClientList = () => {
       <img alt="databiz" src="/images/client-databiz.svg" />
       <img alt="audiophile" src="/images/client-audiophile.svg" />
       <img alt="meet" src="/images/client-meet.svg" />
-      <img alt="marker" src="/images/client-markersvg" />
+      <img alt="marker" src="/public/images/client-maker.svg" />
     </WrapperStyle>
   );
 };
